f2dc4857de445eb2a1e3c1524e66a0758c0092b1
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2017-02-09 17:12:55 +0100

    Cleaned up SVG to DrawBot functions.

--- python/circus/toolbox/bezier/svg2drawbot.py
+++ python/circus/toolbox/bezier/svg2drawbot.py
@@ -4,157 +4,168 @@ import random
 import os.path
 from AppKit import NSPoint
 
-def reflect(p0, p1, p2, p3):
-    u"""Reflects off-curve control point in relation to on-curve one. Used for
-    smooth curves."""
-    px = p2 + (p2 - p0)
-    py = p3 + (p3 - p1)
-    return (px, py)
-
-def contourToPath(contour):
-    u"""Converts SVG contour to a path in DrawBot."""
-    path = BezierPath()
-    p0prev = 0
-    p1prev = 0
-    cp2 = None
-
-    for segment in contour:
-        points = segment[1]
-
-        if segment[0] == 'M':
-            p0, p1 = points
-            p0prev = p0 = float(p0)
-            p1prev = p1 = float(p1)
-            point = (p0, p1)
-            path.moveTo(point)
-        elif segment[0] == 'L':
-            p0, p1 = points
-            p0 = float(p0)
-            p1 = float(p1)
-            p0prev = p0
-            p1prev = p1
-            point = (p0, p1)
-            path.lineTo(point)
-        elif segment[0] == 'l':
-            p0, p1 = points
-            p0 = float(p0) + p0prev
-            p1 = float(p1) + p1prev
-            p0prev = p0
-            p1prev = p1
-            point = (p0, p1)
-            path.lineTo(point)
-        elif segment[0] == 'h':
-            p0 = points[0]
-            p0 = float(p0) + p0prev
-            p1 = p1prev
-            p0prev = p0
-            point = (p0, p1)
-            path.lineTo(point)
-        elif segment[0] == 'H':
-            p0 = points[0]
-            p0 = float(p0)
-            p1 = p1prev
-            p0prev = p0
-            point = (p0, p1)
-            path.lineTo(point)
-        elif segment[0] == 'v':
-            p1 = points[0]
-            p1 = float(p1) + p1prev
-            p0 = p0prev
-            p1prev = p1
-            point = (p0, p1)
-            path.lineTo(point)
-        elif segment[0] == 'V':
-            p1 = points[0]
-            p1 = float(p1)
-            p0 = p0prev
-            p1prev = p1
-            point = (p0, p1)
-            path.lineTo(point)
-        elif segment[0] == 'C':
-            p0 = float(points.pop(0))
-            p1 = float(points.pop(0))
-            p2 = float(points.pop(0))
-            p3 = float(points.pop(0))
-            p4 = float(points.pop(0))
-            p5 = float(points.pop(0))
-            p0prev = p4
-            p1prev = p5
-            cp2 = reflect(p2, p3, p4, p5) # TODO: move to S, s
-            path.curveTo((p0, p1), (p2, p3), (p4, p5))
-
-        elif segment[0] == 'c':
-            p0 = float(points.pop(0)) + p0prev
-            p1 = float(points.pop(0)) + p1prev
-            p2 = float(points.pop(0)) + p0prev
-            p3 = float(points.pop(0)) + p1prev
-            p4 = float(points.pop(0)) + p0prev
-            p5 = float(points.pop(0)) + p1prev
-            p0prev = p4
-            p1prev = p5
-            path.curveTo((p0, p1), (p2, p3), (p4, p5))
-            cp2 = reflect(p2, p3, p4, p5) # TODO: move to S, s
-        elif segment[0] == 's':
-            p0 = float(points.pop(0)) + p0prev
-            p1 = float(points.pop(0)) + p1prev
-            p2 = float(points.pop(0)) + p0prev
-            p3 = float(points.pop(0)) + p1prev
-            p0prev = p2
-            p1prev = p3
-            path.curveTo(cp2, (p0, p1), (p2, p3))
-        elif segment[0] == 'S':
-            p0 = float(points.pop(0))
-            p1 = float(points.pop(0))
-            p2 = float(points.pop(0))
-            p3 = float(points.pop(0))
-            p0prev = p2
-            p1prev = p3
-            path.curveTo(cp2, (p0, p1), (p2, p3))
-    path.closePath()
-    return path
-
-def parseSVG(strings):
-    cmd =['M', 'L', 'l', 'V', 'v', 'C', 'c', 'H', 'h', 'z', 's', 'S']
-    paths = []
-
-    for string in strings:
-        path = []
-        command = None
-        pointstring = ''
-        points = []
-
-        for c in string:
-
-            if c in cmd:
-                #print command, len(points)
-                if  command is not None:
-                    path.append((command, points))
-
-
-                command = c
-                if len(pointstring) > 0:
-                    points.append(pointstring)
-                points = []
-                pointstring = ''
-            else:
-                if c == ' ':
-                    continue
-                if c == ',' or c == '-':
-                    if len(pointstring) > 0:
-                        points.append(pointstring)
-                    if c == '-':
-                        pointstring = c
-                    else:
-                        pointstring = ''
-                else:
-                    pointstring += c
-
-        paths.append(path)
-    return paths
-
 def getSvgPaths(fileName):
+    u"""Extracts path strings from XML."""
     doc = minidom.parse(fileName)  # parseString also exists
     svgPaths = [path.getAttribute('d') for path
                 in doc.getElementsByTagName('path')]
     doc.unlink()
     return svgPaths
+
+def parseSVG(strings):
+    u"""Takes a list of path strings and converts them to a list of SVG-command
+    tuples."""
+    cmd =['m', 'l', 'v', 'c', 'h', 'z', 's']
+
+    paths = []
+
+    for string in strings:
+        command = None   # Current command.
+        valuestring = ''
+        path = []
+        points = []
+
+        for c in string:
+
+            if c.lower() in cmd:
+                # New command, add previous one to path.
+                if  command is not None:
+                    path.append((command, points))
+
+                command = c
+                addValueToPoints(valuestring, points)
+
+                # Reset.
+                points = []
+                valuestring = ''
+            else:
+                # Skip spaces.
+                if c == ' ':
+                    continue
+
+                # New value.
+                if c == ',' or c == '-':
+                    addValueToPoints(valuestring, points)
+
+                    # Split on minus.
+                    if c == '-':
+                        valuestring = c
+                    else:
+                        valuestring = ''
+                else:
+                    valuestring += c
+
+        paths.append(path)
+    return paths
+
+def reflect(point0, point1):
+    u"""Reflects off-curve control point in relation to on-curve one. Used for
+    smooth curves."""
+    px = point1[0] + (point1[0] - point0[0])
+    py = point1[1] + (point1[1] - point0[1])
+    return (px, py)
+
+def getRelative(points, pPrevious):
+    u"""Calculates absolute coordinates by adding the previous point to the
+    relative coordinates."""
+    newPoints = []
+
+    for p in points:
+        newP = (p[0] + pPrevious[0], p[1] + pPrevious[1])
+        newPoints.append(newP)
+
+    return newPoints
+
+def copyPoint(previousPoint, currentPoint):
+    u"""Deep copies previous point."""
+    previousPoint[0] = currentPoint[0]
+    previousPoint[1] = currentPoint[1]
+
+def contourToPath(contour):
+    u"""Converts SVG contour to a path in DrawBot."""
+    path = BezierPath()
+    pPrev = [0.0, 0.0]
+    pPrev2 = None
+    previousCommand = None
+
+    for segment in contour:
+        command = segment[0]
+        points = segment[1]
+
+        relative = False
+
+        if command.islower():
+            relative = True
+
+        command = command.lower()
+
+        if command == 'm':
+            if relative:
+                points = getRelative(points, pPrev)
+
+            path.moveTo(points[0])
+        elif command in ('l', 'h', 'v'):
+            if command == 'l':
+                if relative:
+                    points = getRelative(points, pPrev)
+            elif command == 'h':
+                if relative:
+                    points[0][0] += pPrev[0]
+
+                points[0].append(pPrev[1])
+            elif command == 'v':
+                points[0].insert(0, pPrev[0])
+
+                if relative:
+                    points[0][1] += pPrev[1]
+
+            path.lineTo(points[0])
+        elif command == 'c':
+            if relative:
+                points = getRelative(points, pPrev)
+
+            path.curveTo(*points)
+        elif command == 's':
+            if relative:
+                points = getRelative(points, pPrev)
+
+            # TODO: From the spec:
+            #
+            # If there is no previous command or if the previous command was
+            # not an C, c, S or s, assume the first control point is coincident
+            # with the current point
+            if previousCommand in ('c', 's'):
+                cp = reflect(pPrev2, pPrev)
+            else:
+                print 'TODO: implement, test'
+                #copyPoint(cp, points[0])
+            path.curveTo(cp, *points)
+
+        elif command == 'z':
+            continue
+
+        copyPoint(pPrev, points[-1])
+
+        if command in ('c', 's'):
+            pPrev2 = [0.0, 0.0]
+            copyPoint(pPrev2, points[-2])
+        else:
+            pPrev2 = None
+
+        previousCommand = command
+
+    path.closePath()
+    return path
+
+def addValueToPoints(valuestring, points):
+    u"""Adds the collected character string to the last coordinate in the points
+    list."""
+    if len(valuestring) == 0:
+        return
+
+    value = float(valuestring)
+
+    if len(points) == 0 or len(points[-1]) == 2:
+        points.append([])
+
+    points[-1].append(value)
